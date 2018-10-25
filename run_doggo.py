d794fe1a99921f0c4358a07ff67be06227a58d08
Nathan Kau <nathankau@gmail.com>
2018-10-24 18:47:24 -0700

    initial commit of script to run doggo simulation and modified half-cheetah class which should be put in your gym/gym/envs/mujoco folder

---
+++ run_doggo.py
@@ -0,0 +1,19 @@
+import gym
+import time
+env = gym.make('HalfCheetah-v2')
+env.reset()
+import numpy as np
+
+tf = 10
+dt = 0.01
+env.render()
+input("Press Enter to stop...")
+print(env.action_space.sample().shape)
+
+for i in range(int(tf/dt)):
+	action = np.ones_like(env.action_space.sample())
+
+	env.step(action)
+	env.render()
+	print('fuck you Nathan let\'s use git')
+	print('you;ll thank me later mofo')
