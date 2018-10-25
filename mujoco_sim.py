472eddfdfa8eb5dd2a4dce3d6a988ae91039fa9e
Nathan Kau <nathankau@gmail.com>
2018-10-24 23:57:13 -0700

    Added xml that should go in gym/gym/envs/mujoco/assets. Added script to run mujoco only.

---
+++ mujoco_sim.py
@@ -0,0 +1,6 @@
+import mujoco_py
+model = mujoco_py.load_model_from_path("/Users/nathan/Documents/gym/gym/envs/mujoco/assets/half_cheetah.xml")
+sim = mujoco_py.MjSim(model)
+viewer = mujoco_py.MjViewer(sim)
+for i in range(5000):
+	viewer.render()
--- run_doggo.py
+++ run_doggo.py
@@ -7,13 +7,16 @@ import numpy as np
 tf = 10
 dt = 0.01
 env.render()
-input("Press Enter to stop...")
+# input("Press Enter to stop...")
 print(env.action_space.sample().shape)
 
 for i in range(int(tf/dt)):
 	action = np.ones_like(env.action_space.sample())
+	none = np.zeros_like(env.action_space.sample())
 
-	env.step(action)
+	env.step(none)
 	env.render()
-	print('fuck you Nathan let\'s use git')
-	print('you;ll thank me later mofo')
+	time.sleep(0.002)
+
+	# print('fuck you Nathan let\'s use git')
+	# print('you;ll thank me later mofo')
