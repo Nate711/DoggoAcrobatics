4223b5448e99fc51d626c3b03932a8465d9fce4a
Nathan Kau <nathankau@gmail.com>
2018-12-05 21:13:35 +0000

    Testing increased y pos weight and inclusino of y in obs space

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-python xml_parser.py ; python -m spinup.run ppo --hid "[128, 128]" --env HalfCheetah-v2 --exp_name doggo_backflip_ppo_thresh --gamma 0.99 --max_ep_len 800 --steps_per_epoch 4000 --epochs 2000
+python xml_parser.py ; python -m spinup.run ppo --hid "[128, 128]" --env HalfCheetah-v2 --exp_name doggo_backflip_ppo_ypos --gamma 0.99 --max_ep_len 800 --steps_per_epoch 4000 --epochs 2000
 
